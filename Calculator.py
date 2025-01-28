79cd6aef75b41200a3f31faf0419d41ad4c3c9c2
Mustafaakgull <mstafa.akgul03@gma>
2025-01-28 03:48:33 +0300

    subtraction method added

--- Calculator.py
+++ Calculator.py
@@ -3,3 +3,7 @@ import math
 # /*toplama metodu*/
 def add(a: float, b: float) -> float:
     return math.fsum([a, b])
+
+# /*çıkarma metodu*/
+def subtraction(a: float, b: float) -> float:
+    return a - b
