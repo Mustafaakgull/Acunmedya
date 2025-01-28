eff1899d7fcb56e1945758faedf61d0d6d700230
Mustafaakgull <mstafa.akgul03@gma>
2025-01-28 03:49:14 +0300

    multiply and division methods added.

--- Calculator.py
+++ Calculator.py
@@ -7,3 +7,13 @@ def add(a: float, b: float) -> float:
 # /*çıkarma metodu*/
 def subtraction(a: float, b: float) -> float:
     return a - b
+
+# /*çarpma metodu*/
+def multiply(a: float, b: float) -> float:
+    return math.prod([a, b])
+
+# /*bölme metodu*/
+def division(a: float, b: float) -> float:
+    if b == 0:
+        raise ValueError("Division by zero is not allowed!")
+    return a / b
